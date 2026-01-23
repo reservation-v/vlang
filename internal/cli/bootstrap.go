81831413b867f68deec717b4726b271d364741dd
phobos <phobos@altlinux.org>
2026-01-23 16:13:31 +0300

    Add vendor flag (enable/disable vendoring)

--- internal/cli/bootstrap.go
+++ internal/cli/bootstrap.go
@@ -9,40 +9,32 @@ import (
 	"github.com/reservation-v/vlang/internal/bootstrap"
 )
 
-func parseBootstrapFlags(args []string) (string, error) {
-	fs := flag.NewFlagSet("bootstrap", flag.ContinueOnError)
-	fs.SetOutput(os.Stderr)
-
-	dirPtr := fs.String("dir", ".", "upstream project directory")
-	if err := fs.Parse(args); err != nil {
-		return "", err
-	}
-
-	return *dirPtr, nil
-}
-
 func RunBootstrap(args []string) error {
-	dir, err := parseBootstrapFlags(args)
+	bootstrapFlags, err := parseBootstrapFlags(args)
 	if err != nil {
 		return fmt.Errorf("bootstrap parse flags: %w", err)
 	}
 
-	hadVendorBefore, err := bootstrap.Vendor(dir)
-	if err != nil {
-		return fmt.Errorf("vendor: %w", err)
-	}
-	if hadVendorBefore {
-		fmt.Println("vendor updated")
-	} else {
-		fmt.Println("vendor created")
+	if bootstrapFlags.Vendor == true {
+		hadVendorBefore, err := bootstrap.Vendor(bootstrapFlags.Dir)
+		if err != nil {
+			return fmt.Errorf("vendor: %w", err)
+		}
+		if hadVendorBefore {
+			fmt.Println("vendor updated")
+		} else {
+			fmt.Println("vendor created")
+		}
+	} else if bootstrapFlags.Vendor != false {
+		return fmt.Errorf("unknown vendor flag: %q", bootstrapFlags.Vendor)
 	}
 
-	projectInfo, err := bootstrap.Inspect(dir)
+	projectInfo, err := bootstrap.Inspect(bootstrapFlags.Dir)
 	if err != nil {
 		return fmt.Errorf("inspect: %w", err)
 	}
 
-	projectInfo.Dir, err = filepath.Abs(dir)
+	projectInfo.Dir, err = filepath.Abs(bootstrapFlags.Dir)
 	if err != nil {
 		return fmt.Errorf("bootstrap absolute path: %w", err)
 	}
@@ -50,4 +42,27 @@ func RunBootstrap(args []string) error {
 	fmt.Printf("Project info: %+v\n", projectInfo)
 
 	return nil
+}
+
+type BootstrapFlags struct {
+	Dir    string
+	Vendor bool
+}
+
+func parseBootstrapFlags(args []string) (BootstrapFlags, error) {
+	fs := flag.NewFlagSet("bootstrap", flag.ContinueOnError)
+	fs.SetOutput(os.Stderr)
+
+	dirPtr := fs.String("dir", ".", "upstream project directory")
+	needVendor := fs.Bool("vendor", true, "enable/disable vendoring (yes/no)")
+	if err := fs.Parse(args); err != nil {
+		return BootstrapFlags{}, err
+	}
+
+	bsFlags := BootstrapFlags{
+		Dir:    *dirPtr,
+		Vendor: *needVendor,
+	}
+
+	return bsFlags, nil
 }
